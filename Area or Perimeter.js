c37dd7a318cd8b9820babe1e14f4c46d21e5ec11
Austin <scentedpine0@gmail.com>
2022-07-01 16:52:59 -0600

    Checks if square or rectangle then returns area or permiter respectively

---
+++ Area or Perimeter.js
@@ -0,0 +1,7 @@
+//https://www.codewars.com/kata/5ab6538b379d20ad880000ab/train/javascript
+const areaOrPerimeter = function(l , w) {
+    if (l === w ) 
+      return l * w
+    
+    return (l * 2) + ( w * 2);
+  };
