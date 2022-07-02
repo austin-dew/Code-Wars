c944c2c0b859ec34498c7b9e72ee5c0792a1946a
Austin <scentedpine0@gmail.com>
2022-07-02 11:39:02 -0600

    checks first letter of name to determing if playing banjo

---
+++ Are You Playing Banjo.js
@@ -0,0 +1,4 @@
+//https://www.codewars.com/kata/53af2b8861023f1d88000832/train/javascript
+function areYouPlayingBanjo(name) {
+    return name[0].toLowerCase() === 'r' ? `${name} plays banjo` : `${name} does not play banjo`;
+  }
