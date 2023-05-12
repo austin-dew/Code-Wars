5910a28cd48bea3378958ae24f16b9b2c34be4d5
Austin <scentedpine0@gmail.com>
2023-05-11 20:59:53 -0400

    Writes num in expanded form

---
+++ Write Number in Expanded Form.js
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/5842df8ccbd22792a4000245/train/javascript
+const expandedForm = n => n.toString()
+                            .split("")
+                            .reverse()
+                            .map( (a, i) => a * Math.pow(10, i))
+                            .filter(a => a > 0)
+                            .reverse()
+                            .join(" + ");
