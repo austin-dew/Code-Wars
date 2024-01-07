8db48d55f6eed1ab97be2c51d7f29b6f42cadb99
Austin <scentedpine0@gmail.com>
2024-01-07 12:29:01 -0500

    removed unused variable

--- Two Sum.js
+++ Two Sum.js
@@ -1,7 +1,6 @@
 //https://www.codewars.com/kata/52c31f8e6605bcc646000082/train/javascript
 function twoSum(numbers, target) {
-    let two = [];
-    
+
     for(let i = 0; i < numbers.length; i++) {
       for(let j = 0; j < numbers.length - 1; j++) {
         if(numbers[i] + numbers[j + 1] === target) {
