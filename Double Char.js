c5801d4323f743c46b6f60600cd6e4f7fd765d9d
Austin <scentedpine0@gmail.com>
2024-01-18 11:13:02 -0500

    doubles each char in a string

---
+++ Double Char.js
@@ -0,0 +1,6 @@
+//https://www.codewars.com/kata/56b1f01c247c01db92000076/train/javascript
+function doubleChar(str) {
+    // Your code here
+    return str.split('').map(c => c + c).join('');
+  }
+  
