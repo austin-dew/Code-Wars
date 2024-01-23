11c97ddcddd94353d706927a664715d95e80a1ca
Austin <scentedpine0@gmail.com>
2024-01-23 10:06:21 -0500

    function that does math

---
+++ Make a function that does arithmetic.js
@@ -0,0 +1,15 @@
+//https://www.codewars.com/kata/583f158ea20cfcbeb400000a/train/javascript
+function arithmetic(a, b, operator){
+    switch(operator) {
+        case "add":
+        return a + b;
+        case "subtract":
+        return a - b;
+        case "multiply":
+        return a * b;
+        case "divide":
+        return a / b;
+  
+    }
+    return a + b;
+  }
